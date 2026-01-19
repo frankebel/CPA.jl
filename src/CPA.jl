de7f1d33b18d9d94fa05038b4ebf657b5ec87994
Frank Ebel <frank.ebel@student.tuwien.ac.at>
2026-01-19 14:49:29 +0100

    feat: calculate 2d non-interacting dispersion

--- src/CPA.jl
+++ src/CPA.jl
@@ -3,8 +3,10 @@ module CPA
 export
     # types
     # functions
+    dispersion_2d,
     ibz_2d
 
 include("brillouin_zone.jl")
+include("dispersion.jl")
 
 end
---
+++ src/dispersion.jl
@@ -0,0 +1,37 @@
+# dispersion relation
+
+"""
+    struct Dispersion{R <: Real}
+
+Represent the dispersion relation in the irreducible Brillouin zone (IBZ) together
+with its multiplicity.
+"""
+struct Dispersion
+    energy::Vector{Float64}
+    multiplicity::Vector{Int}
+
+    function Dispersion(energy, multiplicity)
+        length(energy) == length(multiplicity) ||
+            throw(ArgumentError("mismatch list of energies and their associated multiplicity"))
+        return new(energy, multiplicity)
+    end
+end
+
+"""
+    dispersion_2d(ibz::IBZ; μ::Real = 0, t::Real = 1)
+
+Calculate the dispersion relation
+
+```math
+ϵ_k = -2t \\left( \\cos(k_x) + \\cos(k_y) \\right)
+```
+
+for the given irreducible Brillouin zone `ibz`.
+"""
+function dispersion_2d(ibz::IBZ; μ::Real = 0, t::Real = 1)
+    energy = map(k -> energy_2d(k, t), ibz.kgrid)
+    energy .-= μ
+    return Dispersion(energy, ibz.multiplicity)
+end
+
+energy_2d(k::NTuple{2, <:Real}, t::Real = 1) = -2 * t * (cos(k[1]) + cos(k[2]))
