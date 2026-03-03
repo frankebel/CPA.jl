dcb056162010c9000fe4bf260ac23eba663689a8
Frank Ebel <frank.ebel@student.tuwien.ac.at>
2026-03-03 17:08:22 +0100

    refactor: resolvent calculation
    Play around with logarithmic grid which gives much better results.

--- scripts/g_resolvent.jl
+++ scripts/g_resolvent.jl
@@ -8,27 +8,32 @@ using RAS_DMFT
 
 # %%
 # dispersion relation
-grid = range(-10; stop = 10, length = 101)
-n = 100
-ϵ = 8.0
+g = grid_log(10.0, 1.04, 400)
+grid = [-reverse(g); 0; g]
+# grid = range(-10; stop = 10, length = 801)
+n = 1000
+ϵ = 6.2
 μ = ϵ / 2
 x = 0.5
 
+# non-interacting GF
 ibz = ibz_2d(n)
 dispersion = dispersion_2d(ibz; μ)
 G0 = PolesSum(dispersion)
+G0 = to_grid(G0, grid)
+merge_small_weight!(G0, sqrt(eps()))
 
 # %%
 # CPA
-maxiter = 100
+maxiter = 20
 G_loc1, Σ_H1, Σ1 = cpa_loop_resolvent(G0, grid; x, ϵ, maxiter)
-G_loc2, Σ2 = cpa_loop(dispersion, grid; x, ϵ, maxiter);
+G_loc2, Σ2 = cpa_loop(dispersion, grid; x, ϵ);
 
 # %%
 # plot it
 W = range(-10; stop = 10, length = 2001);
 σ = δ = 5.0e-2;
-f = Figure(; size = (1600, 900));
+f = Figure(; size = (1600 / 2, 900 / 2));
 ax = Axis(
     f[1, 1];
     xlabel = L"ω/t",
@@ -41,11 +46,11 @@ axislegend(ax; position = :lt);
 ax = Axis(
     f[1, 2];
     xlabel = L"ω/t",
-    ylabel = L"\mathrm{Im}~\Sigma(\omega)",
+    ylabel = L"-\mathrm{Im}~\Sigma(\omega)",
     #=xticks = -4:2:4,=#
 );
-lines!(ax, W, -imag(evaluate_lorentzian(Σ1, W, δ)) ./ π; label = "resolvent");
-lines!(ax, grid, -imag(Σ2) ./ π; label = "finite broadening");
+lines!(ax, W, -imag(evaluate_lorentzian(Σ1, W, δ)); label = "resolvent");
+lines!(ax, grid, -imag(Σ2); label = "finite broadening");
 axislegend(ax; position = :lt);
 
 display(f)
