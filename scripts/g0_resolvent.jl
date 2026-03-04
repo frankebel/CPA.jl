9993316c0bf83d7562a4b6326e941ea0243d98a8
Frank Ebel <frank.ebel@student.tuwien.ac.at>
2026-03-04 19:20:19 +0100

    feat: export data

--- scripts/g0_resolvent.jl
+++ scripts/g0_resolvent.jl
@@ -3,41 +3,55 @@
 # %%
 # load modules
 using CPA
+using CSV
 using CairoMakie
+using DataFrames
 using RAS_DMFT
 
 # %%
 # dispersion relation
-grid = range(-5; stop = 5, length = 10_001)
-n = 1000
+n = 10
 ibz = ibz_2d(n)
 dispersion = dispersion_2d(ibz)
 
 # %%
 # non-interacting GF
 G0 = PolesSum(dispersion)
-G0 = to_grid(G0, grid)
 merge_small_weight!(G0, sqrt(eps()))
+merge_degenerate_poles!(G0, 1.0e-4)
 
 # %%
 # plot it
-W0 = range(-5; stop = 5, length = 2001)
-W = range(-5; stop = 5, length = 2000) # without ω = 0
-σ = 5.0e-3
-δ = 5.0e-3
-b = 0.4
+W = range(-5; stop = 5, length = 2001)
+σ = δ = 2.0e-1
 f = Figure();
 ax = Axis(
     f[1, 1];
+    xlabel = L"ω/t",
+    ylabel = L"residues of $G_0$",
+    xticks = -4:2:4,
+)
+plot!(ax, locations(G0), weights(G0))
+ax = Axis(
+    f[1, 2];
     xlabel = L"ω/t",
     ylabel = L"A_0(\omega)",
     xticks = -4:2:4,
 )
 lines!(ax, W0, -imag(evaluate_gaussian(G0, W0, σ)) ./ π; label = L"Gaussian, $\sigma=%$σ$")
 lines!(ax, W0, -imag(evaluate_lorentzian(G0, W0, σ)) ./ π; label = L"Lorentzian, $\delta=%$δ$")
-lines!(ax, W, spectral_function_loggaussian(G0, W, b); label = L"log Gaussian, $b=%$b$")
 axislegend(ax; position = :lt)
+f
 
-save("g0_resolvent.pdf", f)
+# %%
+# export
+df = DataFrame()
+df[!, "locations"] = locations(G0)
+df[!, "weights"] = weights(G0)
+CSV.write("g0.csv", df)
 
-return nothing
+df = DataFrame()
+df[!, "W"] = W
+df[!, "gaussian"] = -imag(evaluate_gaussian(G0, W, σ)) ./ π
+df[!, "lorentzian"] = -imag(evaluate_lorentzian(G0, W, δ)) ./ π
+CSV.write("g0_broadened.csv", df)
